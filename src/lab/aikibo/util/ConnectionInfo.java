1878281cdb738fe512a20e3c3c658f7b8d2db089
P. TAMAMI <tamami.oka@gmail.com>
2014-05-28 07:20:06 +0700

    perubahan URL server untuk production

--- src/lab/aikibo/util/ConnectionInfo.java
+++ src/lab/aikibo/util/ConnectionInfo.java
@@ -3,8 +3,8 @@ package lab.aikibo.util;
 public class ConnectionInfo {
 	
 	//public static final String NAMESPACE = "http://service.aikibo.lab";
-	public static final String URL_GET_SPPT = "http://118.96.129.161/PBB-WS-v1/service";
-	public static final String URL_LOCAL_PBBINFO = "http://10.0.0.2:8080/PBB-WS-v2-local/service";
+	public static final String URL_GET_SPPT = "http://118.96.129.161/PBB-WS-v2/service";
+	public static final String URL_LOCAL_PBBINFO = "http://10.0.2.2:8080/PBB-WS-v2-local/service";
 	public static final String URL_KANTOR_PBBINFO = "http://192.168.2.47/PBB-WS-v2/service";
 	//public static final String SOAP_ACTION = "http://aikibo.lab/PBBService";
 	//public static final String SOAP_ACTION_FOR_REALISASI_SPPT_NOW = "http://service.aikibo.lab/PBBService/getRealisasiSPPTByNow";
